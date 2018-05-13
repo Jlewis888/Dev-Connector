e95fdb0e669c8e645a9c9292325a18febb61ddf6
Infinite88 <jlew2024@yahoo.com>
2018-05-13 03:20:01 -0400

    Finished backend

--- server.js
+++ server.js
@@ -4,30 +4,33 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profiles = require('./routes/api/profile');
+const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-mongoose.connect(db)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+// Passport middleware
 app.use(passport.initialize());
 
+// Passport Config
 require('./config/passport')(passport);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
+// Use Routes
 app.use('/api/users', users);
-app.use('/api/profile', profiles);
+app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
